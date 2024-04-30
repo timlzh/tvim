0a6c730e4b75c2ee404626bd863871f27e1d75a0
Timlzh <2921349622@qq.com>
2024-04-30 15:06:21 +0800

    doc: README; fix: fix some errors

--- README.md
+++ README.md
@@ -4,8 +4,20 @@ A neovim configuration based on [LazyVim](https://github.com/LazyVim/LazyVim) al
 
 ## Usage
 
+### Automatic Installation (Recommended)
+
+This script will install `neovide`, `neovim 0.9.5`, `Sarasa Mono SC Nerd Font`, `Cascadia Code Nerd Font` for you automatically and set up the configuration. *This script is WSL2 compatible.*
+
+```bash
+curl -SsL https://raw.githubusercontent.com/timlzh/tvim/main/initNeovim.sh -o /tmp/tvim.sh
+chmod +x /tmp/tvim.sh
+/tmp/tvim.sh
+neovide # neovide.exe in WSL2
+```
+
+### Manual Installation
+
 ```bash
 git clone https://github.com/timlzh/tvim.git ~/.config/nvim
 ln -sf ~/.config/nvim/neovide/config.toml ~/.config/neovide/config.toml
 ```
-
--- initNeovim.sh
+++ initNeovim.sh
@@ -59,12 +59,12 @@ if [ $IS_WSL = true ]; then
 	wget https://github.com/neovide/neovide/releases/download/0.12.2/neovide.exe.zip -O /tmp/neovide.zip
 	NEOVIDE_PATH=${WINDOWS_USER_FOLDER}/AppData/Local/Programs/neovide
 	mkdir -p ${NEOVIDE_PATH}
-	unzip /tmp/neovide.zip -d ${NEOVIDE_PATH}
+	unzip -o /tmp/neovide.zip -d ${NEOVIDE_PATH}
 	rm -rf /tmp/neovide.zip
 
 	addPathToWindows "\$ENV:USERPROFILE\\AppData\\Local\\Programs\\neovide"
 	addPathToWindows $(wslpath -w ~/.local/bin)
-	info "Neovide installed at ${NEOVIDE_PATH} and added to windows PATH. Use 'neovide.exe --wsl' to run."
+	info "Neovide installed at ${NEOVIDE_PATH} and added to windows PATH."
 else
 	# Install Neovide for Linux
 	info "Installing Neovide for Linux..."
@@ -80,6 +80,7 @@ fi
 
 # init neovim
 info "Downloading nvim config"
+rm -rf ~/.config/nvim.bak
 mv ~/.config/nvim ~/.config/nvim.bak
 git clone https://github.com/timlzh/tvim ~/.config/nvim
 
@@ -87,10 +88,13 @@ git clone https://github.com/timlzh/tvim ~/.config/nvim
 if [ $IS_WSL = true ]; then
 	NEOVIDE_CONFIG_PATH=${WINDOWS_USER_FOLDER}/AppData/Roaming/neovide
 else
-	NOEVIDE_CONFIG_PATH=~/.config/neovide
+	NEOVIDE_CONFIG_PATH=~/.config/neovide
 fi
-mkdir -p ${NOEVIDE_CONFIG_PATH}
-ln -sf ~/.config/nvim/neovide/config.toml ${NOEVIDE_CONFIG_PATH}/config.toml
+mkdir -p ${NEOVIDE_CONFIG_PATH}
+cp ~/.config/nvim/neovide/config.toml ${NEOVIDE_CONFIG_PATH}/config.toml
+if [ $IS_WSL = true ]; then
+	echo -e "wsl=true\nneovim_bin=\"$HOME/.local/bin/nvim\"\n$(/bin/cat ${NEOVIDE_CONFIG_PATH}/config.toml)" >${NEOVIDE_CONFIG_PATH}/config.toml
+fi
 
 # Download Sarasa Nerd Font
 style="mono"
@@ -115,32 +119,22 @@ wget $url -O "/tmp/CascadiaCode.zip"
 info "Installing fonts..."
 if [ $IS_WSL = false ]; then
 	# Install the fonts
-	unzip -d /tmp "/tmp/${sarasaFontFileName}"
+	unzip -o -d /tmp "/tmp/${sarasaFontFileName}"
 
 	find -L /tmp -name $pattern 2>/dev/null | cut -d '/' -f 3 | xargs -I {} rm -f ${fontDir}/{}
 	mkdir -p $fontDir
 	find -L /tmp -name $pattern 2>/dev/null | xargs -I {} mv {} ${fontDir}/
 
-	unzip -d ${fontDir} "/tmp/CascadiaCode.zip"
+	unzip -o -d ${fontDir} "/tmp/CascadiaCode.zip"
 
 	rm -r /tmp/*.zip
 	rm -r /tmp/*.ttf
 	fc-cache -r
-
-	# install kitty
-	sudo apt install -y kitty
-
-	# if gnome, change terminal to kitty
-	if [ -n "$DESKTOP_SESSION" ]; then
-		if [ "$DESKTOP_SESSION" = "gnome" ]; then
-			gsettings set org.gnome.desktop.default-applications.terminal exec kitty
-		fi
-	fi
 else
 	mkdir -p ${WINDOWS_USER_FOLDER}/Documents/Fonts/Sarasa
-	unzip -d ${WINDOWS_USER_FOLDER}/Documents/Fonts/Sarasa "/tmp/${sarasaFontFileName}"
+	unzip -o -d ${WINDOWS_USER_FOLDER}/Documents/Fonts/Sarasa "/tmp/${sarasaFontFileName}"
 	mkdir -p ${WINDOWS_USER_FOLDER}/Documents/Fonts/CascadiaCode
-	unzip -d ${WINDOWS_USER_FOLDER}/Documents/Fonts/CascadiaCode "/tmp/CascadiaCode.zip"
+	unzip -o -d ${WINDOWS_USER_FOLDER}/Documents/Fonts/CascadiaCode "/tmp/CascadiaCode.zip"
 	rm -f /tmp/*.zip
 
 	font="$(getWindowsFonts)"
