7447571ef1046cb82a66bbcd8256d2d856670ab3
Timlzh <admin@timlzh.com>
2024-09-25 15:10:36 +0800

    20240925

--- init.lua
+++ init.lua
@@ -1,2 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
+-- only set if NEOVIDE_CWD is set
+if vim.fn.exists("neovide") == 1 and vim.fn.getenv("NEOVIDE_CWD") then
+  vim.fn.chdir(vim.fn.getenv("NEOVIDE_CWD"))
+end
 require("config.lazy")
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,10 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- LSP Server to use for Python.
+-- Set to "basedpyright" to use basedpyright instead of pyright.
+vim.g.lazyvim_python_lsp = "basepyright"
+vim.g.lazyvim_python_ruff = "ruff_lsp"
+-- set shiftwidth to 4
+vim.opt.shiftwidth = 4
